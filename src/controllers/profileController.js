8c0d42024f799cc27d0d46d303700134cb907536
AnanthuTD <ananthutd2021@gmail.com>
2024-11-11 00:13:57 +0530

    feat: implemented profile controller.
    - added method to fetch profile of an authenticated user.

---
+++ src/controllers/profileController.js
@@ -0,0 +1,35 @@
+import { UserRepository } from '../repositories/UserRepository.js'
+
+export class ProfileController {
+    userRepository = new UserRepository()
+
+    getProfile = async (req, res) => {
+        const userId = req.user.id
+
+        if (!userId) {
+            return res
+                .status(401)
+                .json({ message: 'No authenticated user found!' })
+        }
+
+        const user = await this.userRepository.findById(userId)
+
+        if (!user) {
+            return res
+                .status(401)
+                .json({ message: 'No authenticated user found' })
+        }
+
+        const userResponseData = {
+            id: user.id,
+            fullName: user.fullName,
+            email: user.email,
+            username: user.username,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+            phone: user.phone,
+        }
+
+        res.json({ user: userResponseData })
+    }
+}
