1a7b2387e1767d6719016f23d7e367a6bdf20069
AnanthuTD <ananthutd2021@gmail.com>
2024-11-11 00:18:23 +0530

    feat: updated to include auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import userRouter from './userRouter.js'
+import authRouter from './authRoutes.js'
+import { authenticate } from '../middleware/authentication.js'
 
 const router = express.Router()
 
-router.use('/user', userRouter)
+router.use('/user', authenticate, userRouter)
+router.use('/auth', authRouter)
 
 export default router
