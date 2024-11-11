a5491f621485c2a06293ae6365aac09d3932b10f
AnanthuTD <ananthutd2021@gmail.com>
2024-11-11 23:05:24 +0530

    fix: corrected context creation.

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -1,15 +1,14 @@
 import { ApolloServer } from '@apollo/server'
-import { context } from './graphql/context.js'
+import { createContext } from './graphql/context.js'
 import { schema } from './graphql/schema.js'
 import { expressMiddleware } from '@apollo/server/express4'
 
 export async function createApolloServer() {
     const apolloServer = new ApolloServer({
         schema,
-        context,
     })
 
     await apolloServer.start()
 
-    return expressMiddleware(apolloServer)
+    return expressMiddleware(apolloServer, { context: createContext })
 }
