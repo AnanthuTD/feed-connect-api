be09c264193f20491d4461f903ed408d9a1dcc37
AnanthuTD <ananthutd2021@gmail.com>
2024-11-12 22:16:07 +0530

    feat: updated authentication middleware to align with both rest api and GraphQL

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,24 +1,47 @@
 import { JWTService } from '../services/jwtServices.js'
 
-export const authenticate = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]
-
-    if (!token) {
-        return res.status(401).json({
-            message:
-                'Authentication failed! Please log in or go to the home page.',
-        })
+class AuthMiddleware {
+    constructor(jwtService = new JWTService()) {
+        this.jwtService = jwtService
     }
 
-    try {
-        const decodedToken = new JWTService().verifyAccessToken(token)
-        req.user = { id: decodedToken.id }
-        next()
-    } catch (error) {
-        console.log('Authentication failed! ', error)
-        return res.status(401).json({
-            message:
-                'Authentication failed! Please log in or go to the home page.',
-        })
+    authenticate(req, res, next) {
+        const token = req.headers.authorization?.split(' ')[1]
+
+        if (!token) {
+            return this._handleUnauthorized(req, res, 'No token provided.')
+        }
+
+        try {
+            const decodedToken = this.jwtService.verifyAccessToken(token)
+            req.user = { id: decodedToken.id }
+            if (next) {
+                // For REST API, continue to the next middleware
+                return next()
+            } else {
+                // For GraphQL, return user context
+                return { user: req.user }
+            }
+        } catch (error) {
+            console.error('Authentication failed!', error)
+            return this._handleUnauthorized(
+                req,
+                res,
+                'Invalid or expired token.'
+            )
+        }
+    }
+
+    _handleUnauthorized(req, res, message) {
+        if (res) {
+            // REST API response
+            return res.status(401).json({
+                message: `Authentication failed: ${message}`,
+            })
+        } else {
+            return null
+        }
     }
 }
+
+export default AuthMiddleware
