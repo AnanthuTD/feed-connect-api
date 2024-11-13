cb727b7ba1c4fb1be515f7e9a828fc56d4d6ff28
AnanthuTD <ananthutd2021@gmail.com>
2024-11-14 01:26:31 +0530

    feat: added PostMutation and GraphQLUpload

--- src/graphql/types/index.js
+++ src/graphql/types/index.js
@@ -1,6 +1,7 @@
+import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs'
 import { Comment } from './comment.js'
 import { Like, LikeQueries } from './like.js'
-import { Post } from './post.js'
+import { Post, PostMutation } from './post.js'
 import { User, UserQueries } from './user.js'
 import NexusPrismaScalars from 'nexus-prisma/scalars'
 
@@ -9,6 +10,8 @@ export default [
     User,
     UserQueries,
     Post,
+    PostMutation,
+    GraphQLUpload,
     Comment,
     Like,
     LikeQueries,
