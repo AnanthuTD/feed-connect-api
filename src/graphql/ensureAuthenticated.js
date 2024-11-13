093e3cd866d61f1eb7f0e89942e4198fff45bbed
AnanthuTD <ananthutd2021@gmail.com>
2024-11-14 03:50:40 +0530

    feat: added a condition to check if the context has been passed or not

--- src/graphql/ensureAuthenticated.js
+++ src/graphql/ensureAuthenticated.js
@@ -1,6 +1,16 @@
 import { GraphQLError } from 'graphql'
 
 export function ensureAuthenticated(context) {
+    if (!context) {
+        console.log('Expected context as parameter!')
+        throw new GraphQLError('Did you forget to pass context', {
+            extensions: {
+                code: 'UNAUTHENTICATED',
+                http: { status: 401 },
+            },
+        })
+    }
+
     if (!context.user) {
         throw new GraphQLError('User is not authenticated', {
             extensions: {
